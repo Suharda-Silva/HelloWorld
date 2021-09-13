f2060a5d4d931d5dd6f2cd98fec51f5ef38a3489
Suharda Silva <suhardapo@gmail.com>
2021-09-13 23:56:14 +0530

    Revoked ngModule - Test 03

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
+//import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +14,8 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
